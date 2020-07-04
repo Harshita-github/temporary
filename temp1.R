43e8ee7a779b1bb8eb991043d6e76444d8322336
Harshita-github <hharshita_be18@thapar.edu>
2020-07-04 17:28:33 +0530

    (not an)Initial commit

--- temp1.R
+++ temp1.R
@@ -1,3 +1,7 @@
 print("Hello there")
 print("This is my new repository")
 #I am certainly doing something wrong here !! :(
+#install.packages("usethis")
+#library("usethis")
+#use_git_config(user.name="Harshita-github",user.email="hharshita_be18@thapar.edu")\
+#now lets hope for the best!
